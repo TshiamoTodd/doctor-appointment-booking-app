293e8aa2f9240ce549e991f228c46d6daf9df845
TshiamoTodd <tshiamo.mokwena19@gmail.com>
2024-03-22 10:12:56 +0200

    Search bar complete

--- app/page.js
+++ app/page.js
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import Hero from "./_components/Hero";
+import CategorySearch from "./_components/CategorySearch";
 
 export default function Home() {
   return (
     <div>
       {/* Hero Section */}
       <Hero/>
+      {/* Searchbar + Categories */}
+      <CategorySearch/>
     </div>
   );
 }
