76b2b62eaea89b354906b5781c2f8cb236e8caf6
TshiamoTodd <tshiamo.mokwena19@gmail.com>
2024-03-22 09:56:45 +0200

    Hero Section complete

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,4 +1,6 @@
+import { Button } from '@/components/ui/button';
 import Image from 'next/image'
+import Link from 'next/link';
 import React from 'react'
 
 function Header() {
@@ -11,17 +13,17 @@ function Header() {
         {
             id: 2,
             name: 'Explore',
-            path: '/'
+            path: '/explore'
         },
         {
             id: 3,
             name: 'Contact Us',
-            path: '/'
+            path: '/contact'
         }
     ];
 
     return (
-        <div>
+        <div className='flex items-center justify-between p-4 shadow-sm'>
             <div className='flex items-center gap-10'>
                 <Image 
                     src="/logo.svg" 
@@ -31,10 +33,17 @@ function Header() {
                 />
                 <ul className='md:flex gap-8 hidden'>
                     {menu.map((item, index) => (
-                        <li key={index}>{item.name}</li>
+                        <Link href={item.path} key={index}>
+                            <li 
+                                className='hover:text-primary cursor-pointer hover:scale-105 transition-all ease-in-out'
+                            >
+                                {item.name}
+                            </li>
+                        </Link>
                     ))}
                 </ul>
             </div>
+            <Button>Get Started</Button>
         </div>
     );
 }
